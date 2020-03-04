a702d0cf1c8e97bbf698ec02b44a46669b885c18
Chuck King <cking@surveycto.com>
2020-03-04 09:08:48 +0000

    v1 of generic code to search through external svg

--- source/script.js
+++ source/script.js
@@ -1,24 +1,59 @@
-// Find the input element
+// References to field elements
 var input = document.getElementById('text-field');
+var defaultPathFill = "rgba(0,0,0,.4)";
+var selectedPathFill = "#047CC2";
+var textColor = "rgba(0,0,0,.4)";
+var svgImage = document.getElementById("svg-image");
+var svgDoc;
+var svgPaths;
 
-// Define what happens when the user attempts to clear the response
-function clearAnswer() {
-    input.innerHTML = '';
-} 
+function clickPath(id) {
+    input.value = id; // set the value of the input box
+    setAnswer(input.value); // update the field's current answer
+    setAllColorsToDefault(); // set all the colors to default
+    setCurrentColorToSelected(id);
+}
 
-// If the field is not marked readonly, then focus on the field and show the on-screen keyboard (for mobile devices)
-function setFocus() {
-    if(!fieldProperties.READONLY){
-        input.focus();
-        if (window.showSoftKeyboard) {
-            window.showSoftKeyboard();
+function setAllColorsToDefault() {
+    svgDoc = svgImage.contentDocument;
+    svgPaths = svgDoc.querySelectorAll('path');
+    for (var i=0; i < this.svgPaths.length; i++) {
+        svgPaths[i].setAttribute("fill", defaultPathFill);
+    }
+}
+
+function setCurrentColorToSelected(id) {
+    currentPath = svgDoc.getElementById(id);
+    currentPath.setAttribute("fill", selectedPathFill);
+}
+
+function setClickablePaths() {
+    svgDoc = svgImage.contentDocument;
+    svgPaths = svgDoc.querySelectorAll('path');
+    for (var i=0; i < this.svgPaths.length; i++) {
+        currentPathId = svgPaths[i].id;
+        svgPaths[i].onclick = function() {
+            clickPath(currentPathId);
         }
     }
 }
 
+// we need to wait for the SVG to load in order to access the IDs of the paths
+window.onload=function() {
+    setAllColorsToDefault();
+	setClickablePaths();
+};
+
+// Define what happens when the user attempts to clear the response
+function clearAnswer() {
+    input.innerHTML = '';
+    setAllColorsToDefault();
+} 
+
 // Save the user's response (update the current answer)
 input.oninput = function() {
     setAnswer(input.value);
+    setAllColorsToDefault();
 }
 
 // check for standard appearance options and apply them
