a5a6f9d05b722b923c915be4c290d761d84bd7ea
wallace <wallace@surveycto.com>
2020-03-04 21:26:04 -0300

    (1) Fixed function to set selected path. (2) Fixed code to highlight selected path when field loads/text input changes. (3) Fixed clearAnswer() function. (3) Code tweaks.

--- source/script.js
+++ source/script.js
@@ -10,58 +10,57 @@ var svgPaths;
 function clickPath(id) {
     input.value = id; // set the value of the input box
     setAnswer(input.value); // update the field's current answer
+    highlightSelectedPath(); // highlight selected path
+}
+
+function highlightSelectedPath() {
     setAllColorsToDefault(); // set all the colors to default
-    setCurrentColorToSelected(id);
+    var currentPath = svgDoc.getElementById(input.value); // get reference to selected element
+    if (currentPath) { // check if element exists
+        currentPath.setAttribute("fill", selectedPathFill); // update color
+    }
 }
 
 function setAllColorsToDefault() {
-    svgDoc = svgImage.contentDocument;
-    svgPaths = svgDoc.querySelectorAll('path');
-    for (var i=0; i < this.svgPaths.length; i++) {
+    for (var i = 0; i < svgPaths.length; i++) {
         svgPaths[i].setAttribute("fill", defaultPathFill);
     }
 }
 
-function setCurrentColorToSelected(id) {
-    currentPath = svgDoc.getElementById(id);
-    currentPath.setAttribute("fill", selectedPathFill);
-}
-
 function setClickablePaths() {
-    svgDoc = svgImage.contentDocument;
-    svgPaths = svgDoc.querySelectorAll('path');
-    for (var i=0; i < this.svgPaths.length; i++) {
-        currentPathId = svgPaths[i].id;
+    for (var i = 0; i < svgPaths.length; i++) {
         svgPaths[i].onclick = function() {
-            clickPath(currentPathId);
+            clickPath(this.id);
         }
     }
 }
 
 // we need to wait for the SVG to load in order to access the IDs of the paths
-window.onload=function() {
-    setAllColorsToDefault();
-	setClickablePaths();
-};
+svgImage.addEventListener('load', function() {
+    svgDoc = svgImage.contentDocument;
+    svgPaths = svgDoc.querySelectorAll('path');
+    highlightSelectedPath();
+    setClickablePaths();
+}, true);
 
 // Define what happens when the user attempts to clear the response
 function clearAnswer() {
-    input.innerHTML = '';
+    input.value = '';
     setAllColorsToDefault();
 } 
 
 // Save the user's response (update the current answer)
 input.oninput = function() {
     setAnswer(input.value);
-    setAllColorsToDefault();
-}
+    highlightSelectedPath();
+};
 
 // check for standard appearance options and apply them
-if ( fieldProperties.APPEARANCE.includes("numbers_phone") == true ) {
+if ( fieldProperties.APPEARANCE.includes("numbers_phone") === true ) {
     input.type = "tel";
-} else if ( fieldProperties.APPEARANCE.includes("numbers_decimal") == true ) {
+} else if ( fieldProperties.APPEARANCE.includes("numbers_decimal") === true ) {
     input.pattern = "[0-9]*";
     input.inputmode = "numeric";
-} else if ( fieldProperties.APPEARANCE.includes("numbers") == true ) {
+} else if ( fieldProperties.APPEARANCE.includes("numbers") === true ) {
     input.type = "number";
 } 
